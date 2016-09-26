077bf781aedea7575659262dfb9c01fad5a970db
codywatters <cowatter@mines.edu>
2016-09-26 00:12:07 -0600

    problem with the 'load', sill needs a lot of work. main loops and ish work

--- dsl/SkiStore.rb
+++ dsl/SkiStore.rb
@@ -1,20 +1,27 @@
+# Cody Watters
+# Peter Palumbo
+# CSCI400 
+
 require 'singleton'
+
+# TODO rename products to something else
 
 class SkiStore
 
+	attr_accessor :current_product, :products
+
 	include Singleton
 
-	def load_rules
-		puts "load the rules TODO"
+	def initialize
+		@products = Hash.new
 	end
 
-	def main_menu
-		puts "____.: Main Menu :.____"
-		puts "1. Load Rules"
-		puts "2. Process Orders"
-		puts "3. End"
-
-		return $stdin.gets.chomp.downcase
+	def add_product(text)
+		@products[text] = Array.new	
+	end
+	
+	def add_action(action)
+		@products[@current_product] << action
 	end
 
 	# Main loop
@@ -41,7 +48,18 @@ class SkiStore
 		end
 	end
 
-	def load_rules
+	def main_menu
+		puts "____.: Main Menu :.____"
+		puts "1. Load Rules"
+		puts "2. Process Orders"
+		puts "3. End"
+
+		return $stdin.gets.chomp.downcase
+	end
+
+	def load_rules 
+
+		# First load the file itself and check if it is valid
 		puts 'Enter filename containing rules: '
 		filename = $stdin.gets.chomp
 
@@ -56,9 +74,18 @@ class SkiStore
 
 		begin
 			load filename 
+			puts ''
 		rescue LoadError => ex
 			puts "Invalid filename: #{ex}"
 		end
+
+		#TODO
+		# here put another rescure for an improper file
+
+		# testing
+		puts "products"
+		puts @products
+
 	end
 
 
@@ -68,17 +95,49 @@ class SkiStore
 		puts ''
 		still_processing = true
 
+		# testing
+		puts "products"
+		puts @products
+
 		while still_processing
 			puts "Enter product type or 'D' (done) to end:"
-			option = gets.chomp.downcase
-			if option == 'd' || option == 'done'
+			product_choice = gets.chomp
+
+			# Break out 
+			if product_choice == 'd' || product_choice == 'done'
 				still_processing = false
+			
+			# Perform each action for the item
 			else
-				puts "todo #{option}"
+				if(SkiStore.instance.products.keys.include? product_choice)
+					@products.fetch(product_choice).each{|action| puts "---- #{action}"}
+				else
+					#TODO this throw error
+					puts "TODO THIS NEEDS TO THROW AN ERROR I THINK"
+					puts "Error: Product not defined"
+				end
+
 			end
 		end
 		puts ''
 	end
+end
 
-	
+
+def product(text)
+	temp = text.chomp
+	if !SkiStore.instance.products.keys.include? temp	#check if duplicate
+		SkiStore.instance.add_product(temp)
+	end
+	SkiStore.instance.current_product = temp
+
+	# TESTING DELETEME
+	puts 'pause product in product'
+	puts @products
+	ind = gets
+end
+
+
+def email (input)
+	SkiStore.instance.add_action("email #{input}")
 end
